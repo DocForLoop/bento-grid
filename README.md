4e2bc4d1f1536c7c67cb480ccf4b1026762455d0
Csaba Molnar <docforloop@proton.me>
2025-01-03 12:09:49 +0100

    Setup basic project structure

---
+++ README.md
@@ -0,0 +1,43 @@
+# Frontend Mentor - Bento grid solution
+
+This is a solution to the [Bento grid challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/bento-grid-RMydElrlOj). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [License](#License)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- [Solution](https://www.frontendmentor.io/solutions/)
+- [Live Site](https://docforloop.github.io/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Sass
+
+## Author
+
+- Frontend Mentor - [@DocForLoop](https://www.frontendmentor.io/profile/DocForLoop)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const { src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browsersync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const rimraf = require('rimraf');
+
+// Compile Sass for development
+function scssDevTask() {
+    return src('scss/style.scss', { sourcemaps: true })
+        .pipe(sass({ includePaths: ['./scss'] }).on('error', sass.logError)) // Add the root SCSS directory for imports
+        .pipe(dest('css', { sourcemaps: '.' }));
+}
+
+// Compile Sass for production
+function scssProdTask() {
+    return src('scss/style.scss')
+        .pipe(sass({ includePaths: ['./scss'] }).on('error', sass.logError))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('docs/css'));
+}
+
+// Clean docs folder
+function cleanDocs(done) {
+    rimraf.sync(['docs']);  
+    done();
+}
+
+// Copy HTML and Assets
+function copyFiles() {
+    return src(['index.html', 'assets/**/*'], { base: '.' })
+        .pipe(dest('docs'));
+}
+
+// Serve and reload
+function browserSyncServe(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+                right: '0',
+                left: 'auto',
+                backgroundColor: '#444',
+                color: '#fff',
+                padding: '10px',
+                borderRadius: '10px',
+                fontSize: '14px',
+                textAlign: 'center',
+            },
+        },
+        // Ensures CSS injection happens without a full page reload
+        injectChanges: true,
+    });
+    cb();
+}
+
+function browserSyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('*.html', browserSyncReload);
+    watch('scss/**/*.scss', series(scssDevTask, browserSyncReload));
+}
+
+// Default Task
+exports.default = series(scssDevTask, browserSyncServe, watchTask);
+
+// Deploy Task
+exports.deploy = series(cleanDocs, scssProdTask, copyFiles);
